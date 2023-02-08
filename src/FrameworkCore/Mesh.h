4b984343a4381adde14a10a9ea93af6cb9e65bb2
zhangzhousuper <52350596+zhangzhousuper@users.noreply.github.com>
2023-02-08 03:53:48 -0700

    [add] hw1 render part

--- src/FrameworkCore/Mesh.h
+++ src/FrameworkCore/Mesh.h
@@ -2,79 +2,80 @@
 #ifndef OPENGLFRAMEWORK_CORE_MESH_H_
 #define OPENGLFRAMEWORK_CORE_MESH_H_
 
+#include "Framebuffer.h"
 #include "Shader.h"
 #include "Texture.h"
-#include "Framebuffer.h"
 
-#include <assimp/scene.h>
 #include <assimp/postprocess.h>
+#include <assimp/scene.h>
 #include <glm/glm.hpp>
 
-#include <unordered_map>
 #include <filesystem>
 #include <functional>
+#include <unordered_map>
 #include <vector>
 
-namespace OpenGLFramework::Core
-{
+namespace OpenGLFramework::Core {
 struct PathHash_AssumeCanonical {
-    size_t operator()(const std::filesystem::path& path) const {
-        return std::filesystem::hash_value(path);
-    }
+  size_t operator()(const std::filesystem::path &path) const {
+    return std::filesystem::hash_value(path);
+  }
 };
-// Key should be absolute path so that absolute & relative path will be seen as one.
+// Key should be absolute path so that absolute & relative path will be seen as
+// one.
 using TexturePool = std::unordered_map<std::filesystem::path, Texture,
-    PathHash_AssumeCanonical>;
+                                       PathHash_AssumeCanonical>;
 
-struct VertexAttribute
-{
-    glm::vec3 normalCoord;
-    glm::vec2 textureCoord;
+struct VertexAttribute {
+  glm::vec3 normalCoord;
+  glm::vec2 textureCoord;
 };
 
-class BasicTriMesh
-{
+class BasicTriMesh {
 public:
-    std::vector<glm::vec3> vertices;
-    std::vector<glm::ivec3> triangles;
-    BasicTriMesh(const aiMesh* mesh);
-    std::vector<glm::vec3> GetRealTriNormals();
-    std::vector<glm::vec3> GetRealVertexNormals();
+  std::vector<glm::vec3> vertices;
+  std::vector<glm::ivec3> triangles;
+  BasicTriMesh(const aiMesh *mesh);
+  std::vector<glm::vec3> GetRealTriNormals();
+  std::vector<glm::vec3> GetRealVertexNormals();
 };
 
-class BasicRenderTriMesh : public BasicTriMesh
-{
+class BasicRenderTriMesh : public BasicTriMesh {
 public:
-    std::vector<VertexAttribute> verticesAttributes;
-    std::vector<std::reference_wrapper<Texture>> diffuseTextureRefs;
-    std::vector<std::reference_wrapper<Texture>> specularTextureRefs;
+  std::vector<VertexAttribute> verticesAttributes;
+  std::vector<std::reference_wrapper<Texture>> diffuseTextureRefs;
+  std::vector<std::reference_wrapper<Texture>> specularTextureRefs;
 
-    BasicRenderTriMesh(const aiMesh* mesh, const aiMaterial* material, 
-        TexturePool& texturePool, const std::filesystem::path& rootPath);
-    BasicRenderTriMesh(const BasicRenderTriMesh&) = delete;
-    BasicRenderTriMesh& operator=(const BasicRenderTriMesh&) = delete;
-    BasicRenderTriMesh(BasicRenderTriMesh&&) noexcept;
-    BasicRenderTriMesh& operator=(BasicRenderTriMesh&&) noexcept;
-    ~BasicRenderTriMesh();
+  BasicRenderTriMesh(const aiMesh *mesh, const aiMaterial *material,
+                     TexturePool &texturePool,
+                     const std::filesystem::path &rootPath);
+  BasicRenderTriMesh(const BasicRenderTriMesh &) = delete;
+  BasicRenderTriMesh &operator=(const BasicRenderTriMesh &) = delete;
+  BasicRenderTriMesh(BasicRenderTriMesh &&) noexcept;
+  BasicRenderTriMesh &operator=(BasicRenderTriMesh &&) noexcept;
+  ~BasicRenderTriMesh();
 
-    void Draw(Shader& shader);
-    void Draw(Shader& shader, Framebuffer& buffer);
+  void Draw(Shader &shader);
+  void Draw(Shader &shader, Framebuffer &buffer);
+
 private:
-    GLuint VAO, VBO, IBO;
-    void AllocateAndMemcpyVerticesData_();
-    void BindVerticesAttributeSequence_();
-    void AllocateAndMemcpyTrianglesData_();
-    void ReleaseRenderResources_();
+  GLuint VAO, VBO, IBO;
+  void AllocateAndMemcpyVerticesData_();
+  void BindVerticesAttributeSequence_();
+  void AllocateAndMemcpyTrianglesData_();
+  void ReleaseRenderResources_();
 
-    void SetupRenderResource_();
-    void CopyAttributes_(const aiMesh* mesh);
-    void AddAllTexturesToPoolAndFillRefs_(const aiMaterial* material,
-        TexturePool& texturePool, const std::filesystem::path& rootPath);
-    void AddTexturesToPoolAndFillRefsByType_(const aiMaterial* material,
-        const aiTextureType type, std::vector<std::reference_wrapper<Texture>>& refs,
-        TexturePool& texturePool, const std::filesystem::path& rootPath);
+  void SetupRenderResource_();
+  void CopyAttributes_(const aiMesh *mesh);
+  void AddAllTexturesToPoolAndFillRefs_(const aiMaterial *material,
+                                        TexturePool &texturePool,
+                                        const std::filesystem::path &rootPath);
+  void AddTexturesToPoolAndFillRefsByType_(
+      const aiMaterial *material, const aiTextureType type,
+      std::vector<std::reference_wrapper<Texture>> &refs,
+      TexturePool &texturePool, const std::filesystem::path &rootPath);
 };
 
-}
+} // namespace OpenGLFramework::Core
 
 #endif // !OPENGLFRAMEWORK_CORE_MESH_H_
